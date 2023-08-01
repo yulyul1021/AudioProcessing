dff296d27b540c3f8f64d02702f8532768917fd1
yulyul102101 <yulyul102101@gmail.com>
2023-08-01 16:06:46 +0900

    index.html 및 views.py 수정

--- audioProcessing/models.py
+++ audioProcessing/models.py
@@ -5,5 +5,5 @@ class AudioData(models.Model):
     original_audio = models.FileField(upload_to='original/', null=True, blank=True)
     original_text = models.TextField(blank=True)
     create_date = models.DateTimeField()
-    processed_text = models.TextField()
+    processed_text = models.TextField(blank=True)
     processed_audio = models.FileField(upload_to='processed/')
--- audioProcessing/views.py
+++ audioProcessing/views.py
@@ -3,8 +3,6 @@ from .forms import AudioForm
 from django.utils import timezone
 from .utils import *
 
-from pydub import AudioSegment
-
 
 def index(request):
     if request.method == 'POST':
@@ -18,12 +16,11 @@ def index(request):
                 audio_file = request.FILES['original_audio']
 
                 # 한국어 음성 -> 텍스트 변환
-                kr_text = audio_to_text(audio_file)  # 녹음된 파일일시 해당 함수에서 오류
-                audio_data.original_text = kr_text
+                kr_text = audio_data.original_text  # 녹음된 파일일시 해당 함수에서 오류
 
                 # 한 -> 영 텍스트 번역
                 en_text = text_translate(kr_text)
-                audio_data.processed_text = en_text
+                # audio_data.processed_text = en_text
 
                 # tts wav file create
                 tts_file = text_to_tts(en_text)
@@ -38,18 +35,6 @@ def index(request):
             elif not request.POST.get('original_text'):
                 # 오디오만 input -> 인식 -> 번역 -> tts
                 audio_file = request.FILES['original_audio']
-
-                # 녹음으로 들어왔을때 데이터 변환(함수로 만들어놓기)
-                if audio_file.name.split('.')[1] == 'webm':
-                    webm_file = audio_file.file
-
-                    webm_audio = AudioSegment.from_file(webm_file, format="webm")
-                    wav_data = webm_audio.export(format="wav")
-
-                    # 정상적으로 저장 및 재생됨(확장자는 webm이지만 file check시 wav로 확인됨)
-                    audio_data.original_audio.save(audio_file.name, wav_data)
-                    audio_file = wav_data
-                #
 
                 # 한국어 음성 -> 텍스트 변환
                 kr_text = audio_to_text(audio_file)  # 녹음된 파일일시 해당 함수에서 오류
